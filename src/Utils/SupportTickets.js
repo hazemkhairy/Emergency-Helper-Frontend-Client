5dd12de0f22955e84588fa211c9c10b3fe3c0166
Alaa Sadek <alaa.sa.mahmoud@gmail.com>
2020-06-02 14:17:51 +0200

    fixed keyboard issue

--- src/Utils/SupportTickets.js
+++ src/Utils/SupportTickets.js
@@ -46,7 +46,7 @@ export const getTicketsMessages = async (id) => {
       for(var item in data){
          arr.push(data[item]);
        }
-      return arr;
+      return arr.reverse();
     })
     .catch((error) => {
       return error;
--- src/screens/TicketScreen.js
+++ src/screens/TicketScreen.js
@@ -1,177 +1,188 @@
-import React,{useState,useEffect} from 'react';
-import {  View,StyleSheet,FlatList,Dimensions,TextInput,TouchableOpacity,KeyboardAvoidingView,TouchableWithoutFeedback,Keyboard } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, StyleSheet, FlatList, Dimensions, TextInput, TouchableOpacity, KeyboardAvoidingView, TouchableWithoutFeedback, Keyboard } from 'react-native';
 import ChatCard from '../components/cardComponents/chatCard'
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
 import MainHeader from '../components/global/MainHeader'
+import KeyboardSpacer from 'react-native-keyboard-spacer'
 
-import {getTicketsMessages,addMessage} from '../Utils/SupportTickets'
+import { getTicketsMessages, addMessage } from '../Utils/SupportTickets'
 
-const TicketScreen = ({navigation}) => {
+const TicketScreen = ({ navigation }) => {
 
-  const category =navigation.state.params.props.category
-  const ticketID=navigation.state.params.props.id
-  const description=navigation.state.params.props.description
-  const date=navigation.state.params.props.date
-  
-  console.log(ticketID)
-  
-  const obj=
+  const category = navigation.state.params.props.category
+  const ticketID = navigation.state.params.props.id
+  const description = navigation.state.params.props.description
+  const date = navigation.state.params.props.date
+
+
+
+  const obj =
   {
-      _id:"1",
-      date:date,
-      message:description,
-      senderRole:"Client"
+    _id: "1",
+    date: date,
+    message: description,
+    senderRole: "Client"
   }
-  
+
   const [messages, setMessages] = useState([]);
-  const [message,setMessage]=useState('');
+  const [message, setMessage] = useState('');
   const [reloading, setReloading] = useState(false);
   const [active, setActive] = useState(false);
-  messages[0]=(obj)
+  messages[0] = (obj)
 
   const newMessage = async () => {
-   if(active==true){
-      addMessage(ticketID,message).then((result) => {
-      setMessage('')
-      setActive(false)
-      getMessages()
-    
-    });
+    if (active == true) {
+      addMessage(ticketID, message).then((result) => {
+        setMessage('')
+        setActive(false)
+        getMessages()
+
+      });
+    }
   }
-  }
-  
+
   const getMessages = async () => {
     setReloading(true);
     setMessages([]);
-    messages.length=1
+    messages.length = 1
     await getTicketsMessages(ticketID).then((result) => {
-      for(var item in result){
-         messages.push(result[item]);
-       }
+      for (var item in result) {
+        messages.push(result[item]);
+      }
       setMessages(messages);
       setReloading(false);
     });
   };
   useEffect(() => {
     getMessages();
-    
+
   }, []);
 
- 
+
   return (
     <View style={styles.container}>
-      <View style={{height:Dimensions.get("window").height *0.81}}>
+      <View style={{ height: Dimensions.get("window").height * 0.81 }}>
 
-       <MainHeader headerText={category} style={{height:Dimensions.get('window').height * 0.18, marginBottom:'4%'}} />
-      
-         <FlatList
-         style={{ flexGrow: 0}}
-             refreshing={reloading}
-             onRefresh={() => getMessages()}
-             data={messages}
-             extraScrollHeight={100}
-             onEndReachedThreshold={0.5}
-             scrollToIndex={messages.length - 1}
-             initialScrollIndex={messages.length - 1}
-             getItemLayout = {(data, index) => (
-              { length: 170, offset: 170 * index, index }
-              )}
-             keyExtractor={(item,index) => 'key'+index}
-             showsVerticalScrollIndicator={true}
-             renderItem={({ item, index }) => {
-            return (
-              <View >
-               <ChatCard item={item} />
-              </View>
-            )
-          }}
-         />
+        <MainHeader headerText={category} style={{ height: Dimensions.get('window').height * 0.18, marginBottom: '4%' }} />
+
+
+        <View style={{ flex: 1 }}>
+
+          <FlatList
+            inverted
+            keyboardShouldPersistTaps="handled"
+            style={{ flex: 1 }}
+            refreshing={reloading}
+            onRefresh={() => getMessages()}
+            data={messages}
+            // extraScrollHeight={100}
+            // onEndReachedThreshold={0.5}
+            // scrollToIndex={messages.length - 1}
+            // initialScrollIndex={messages.length - 1}
+            getItemLayout={(data, index) => ({
+              length: 170,
+              offset: 170 * index,
+              index,
+            })}
+            keyExtractor={(item, index) => "key" + index}
+            showsVerticalScrollIndicator={true}
+            renderItem={({ item, index }) => {
+              return (
+                <View>
+                  <ChatCard item={item} />
+                </View>
+              );
+            }}
+          />
+          <KeyboardSpacer
+          />
         </View>
-       
-        <KeyboardAvoidingView
+      </View>
+
+      <KeyboardAvoidingView
         behavior={'position'}
         style={styles.container}
         keyboardVerticalOffset={60}
-        >
+      >
         <View style={styles.line}></View>
         <View style={styles.footer}>
-        <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()} accessible={false}>
-          <View style={styles.inputContainer}>
-            <TextInput 
+          <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()} accessible={false}>
+            <View style={styles.inputContainer}>
+              <TextInput
                 placeholder="Write your reply…"
                 autoCorrect={false}
-               
+
                 placeholderTextColor='#BCC5D3'
                 underlineColorAndroid='transparent'
-                onChangeText={(text) => {setMessage(text)
+                onChangeText={(text) => {
+                  setMessage(text)
                   {
-                    if(/\S/.test(text))
-                  {
-                    setActive(true)
-                   
-                  } else setActive(false)
+                    if (/\S/.test(text)) {
+                      setActive(true)
+
+                    } else setActive(false)
                   }
-                  }}
+                }}
                 style={styles.input}
                 //returnKeyType="send"
                 value={message}
-               
-                />
-               
-          </View> 
+
+              />
+
+            </View>
           </TouchableWithoutFeedback>
-          {!active?<Icon name={'arrow-right-circle'}  color={'#BCC5D3'} size={30}/>:  
-            <TouchableOpacity onPress={()=>newMessage()}>
-            <Icon name={'arrow-right-circle'}  color={'#132641'} size={30}/> 
-            </TouchableOpacity> }
+          {!active ? <Icon name={'arrow-right-circle'} color={'#BCC5D3'} size={30} /> :
+            <TouchableOpacity onPress={() => newMessage()}>
+              <Icon name={'arrow-right-circle'} color={'#132641'} size={30} />
+            </TouchableOpacity>}
         </View>
-    
-       
-        </KeyboardAvoidingView>
-     
-        </View>
-      
+
+
+      </KeyboardAvoidingView>
+
+    </View>
+
   );
 
 
 }
 const styles = StyleSheet.create({
-  container:{
-    backgroundColor:'#FFFFFF', 
+  container: {
+    backgroundColor: '#FFFFFF',
     height: Dimensions.get('window').height,
     width: Dimensions.get('window').width,
-    flex:1,
-   
+    flex: 1,
+
   },
-  line:{
-      borderWidth:1,
-      borderColor:'#E9EEF1',
+  line: {
+    borderWidth: 1,
+    borderColor: '#E9EEF1',
   },
-  footer:{
+  footer: {
     flexDirection: 'row',
     backgroundColor: '#FFFFFF',
-    paddingHorizontal:10,
-    alignItems:'center',
+    paddingHorizontal: 10,
+    alignItems: 'center',
   },
   inputContainer: {
     flexDirection: 'row',
-    alignItems:'center',
-    flex:1,
-    marginRight:10,
-    height:60
+    alignItems: 'center',
+    flex: 1,
+    marginRight: 10,
+    height: 60
   },
-  input:{
-    fontFamily:'Montserrat',
-    fontSize:16,
-    color:'#BCC5D3',
-    paddingHorizontal:'10%',
+  input: {
+    fontFamily: 'Montserrat',
+    fontSize: 16,
+    color: '#BCC5D3',
+    paddingHorizontal: '10%',
     justifyContent: 'flex-end',
-    bottom:0,
-    height:60,
-    width:'100%',
+    bottom: 0,
+    height: 60,
+    width: '100%',
     position: 'absolute'
   },
- 
+
 })
 
 export default TicketScreen;
