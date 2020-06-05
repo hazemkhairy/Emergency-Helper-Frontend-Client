4d20049e581d0bbadd5d491ee63d8676953a2a92
Aya Eissa <ayaahmedeissa@gmail.com>
2020-06-05 15:32:09 +0200

    Fix adding new support ticket problem

--- src/screens/SupportTicketScreen.js
+++ src/screens/SupportTicketScreen.js
@@ -1,77 +1,79 @@
 import React, { useState, useEffect } from 'react';
-import { Text, View,StyleSheet,Dimensions, FlatList } from 'react-native';
+import { Text, View, StyleSheet, Dimensions, FlatList } from 'react-native';
 import AddCard from '../components/cardComponents/supportTicketCard'
 import Button from '../components/global/reusableButton'
 import AddModal from '../components/cardComponents/AddTicketModal'
 import Icon from 'react-native-vector-icons/Entypo';
 import MainHeader from '../components/global/MainHeader'
 import SubHeaderText from '../components/global/SubHeaderText'
-import {getAllTickets} from '../Utils/SupportTickets'
+import { getAllTickets } from '../Utils/SupportTickets'
 
 const SupportTicket = () => {
-  
   const [modalVisible, setModalVisible] = useState(false);
   const [tickets, setTickets] = useState([]);
-  const [reloading, setReloading] = useState(false);
+  const [isFetching, setIsFetching] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   const getTickets = async () => {
-    setReloading(true);
+    setIsFetching(true);
     setTickets([]);
+    setLoading(true);
     await getAllTickets().then((result) => {
       setTickets(result);
-      setReloading(false);
+      setLoading(false);
+      setIsFetching(false);
     });
+  };
+  // const reload = () => {
+  //   getTickets();
+  // };
+
+  const addNewTicket = () => {
+    setModalVisible(!modalVisible)
+
   };
   const reload = () => {
     getTickets();
   };
 
-  const open=()=>
-  { 
-    setModalVisible(!modalVisible)
-    setReloading(!reloading)
-    if(modalVisible)
-    {
-      setReloading(true)
-    }
-  }
   useEffect(() => {
     getTickets();
   }, []);
 
-    return (
-    <View  style={styles.container}>
-         <AddModal modalVisible={modalVisible}  newItem={() => getTickets()} 
-         />
-        <MainHeader headerText='Support' name={'users'}/>
-          <View style={styles.rowContainer}>
-          <View style={{marginBottom:'5%'}}>
-          <SubHeaderText SubHeaderText={'Tickets'}/>
+  return (
+    <View style={styles.container}>
+      <AddModal modalVisible={modalVisible} newItem={() => getTickets()}
+      />
+      <MainHeader headerText='Support' name={'users'} />
+      <View style={styles.rowContainer}>
+        <View style={{ marginBottom: '5%' }}>
+          <SubHeaderText SubHeaderText={'Tickets'} />
+        </View>
+        <Button
+          onPress={() => addNewTicket()}
+        >
+          <View style={styles.buttonContainer}>
+            <Icon name={'plus'} style={styles.iconStyle} />
+            <Text style={styles.addButton}>New</Text>
           </View>
-          <Button 
-          onPress={() =>open()}
-          >
-            <View style={styles.buttonContainer}>
-             <Icon name={'plus'} style={styles.iconStyle} />
-             <Text style={styles.addButton}>New</Text>
-             </View>
-           </Button>
-          </View>
-          <FlatList
-             data={tickets}
-             extraData={tickets}
-             keyExtractor={(item,index) => 'key'+index}
-             showsVerticalScrollIndicator={false}
-             refreshing={reloading}
-             onRefresh={() => getTickets()}
-             renderItem={({ item }) => {
-            return (
-              <View >
-                <AddCard item={item} />
-              </View>
-            )
-          }}
-         />
+        </Button>
+      </View>
+      {tickets.length ? (
+        <FlatList
+          data={tickets}
+          extraData={tickets}
+          keyExtractor={(item, index) => index.toString()}
+          showsVerticalScrollIndicator={false}
+          refreshing={isFetching}
+          onRefresh={() => getTickets()}
+          renderItem={({ item }) => (
+            <View >
+              <AddCard item={item} />
+            </View>
+          )}
+        />
+      ) : (<Text > </Text>
+        )}
     </View >
 
 
@@ -81,31 +83,32 @@ const SupportTicket = () => {
 }
 
 const styles = StyleSheet.create({
-  container:{
-    backgroundColor:'#FFFFFF', 
+  container: {
+    backgroundColor: '#FFFFFF',
     height: Dimensions.get('window').height,
     width: Dimensions.get('window').width,
-    flex:1
+    flex: 1
   },
-    rowContainer:{
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        width:'85%',
-        alignSelf: 'center',
-        marginTop:'5%',
-    },
-    iconStyle:{
-        fontSize:20,
-        color:'#FFFFFF',
-    },
-    buttonContainer:{
-      flexDirection: 'row',
-    },
-    addButton:{
-      color: "white",
-      fontSize: 18,
-      fontFamily: "Montserrat",
-    }
+  rowContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '85%',
+    alignSelf: 'center',
+    marginTop: '5%',
+  },
+  iconStyle: {
+    fontSize: 20,
+    color: '#FFFFFF',
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+  },
+  addButton: {
+    color: "white",
+    fontSize: 18,
+    fontFamily: "Montserrat",
+  },
+
 })
 
 export default SupportTicket;
