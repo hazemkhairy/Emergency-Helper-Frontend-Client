0a35fb7dde66112b577f02e4dbe3d1feff9f52fe
Mai Sherif <mai.sherif1997@hotmail.com>
2020-06-13 00:58:08 +0200

    fix keyboard and textinput SupportTicket

--- src/screens/TicketScreen.js
+++ src/screens/TicketScreen.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { View, StyleSheet, FlatList, Dimensions, TextInput, TouchableOpacity, KeyboardAvoidingView, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import { View,Text, StyleSheet, FlatList, Dimensions, TextInput, TouchableOpacity, KeyboardAvoidingView, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import normalize from "react-native-normalize";
 import ChatCard from '../components/cardComponents/chatCard'
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
 import MainHeader from '../components/global/MainHeader'
@@ -51,7 +52,9 @@ const TicketScreen = ({ navigation }) => {
   return (
     <View style={styles.container}>
       <View style={{ height: Dimensions.get('window').height < 600 ? Dimensions.get("window").height * 0.75 : Dimensions.get("window").height * 0.90 }}>
-        <MainHeader headerText={category} style={{ height: Dimensions.get('window').height * 0.22 }} />
+       <View style={styles.headerContainer}>
+         <Text style={styles.headerText}> {category} </Text>
+       </View>
         <View style={{ flex: 1 }}>
           <FlatList
             inverted
@@ -80,8 +83,8 @@ const TicketScreen = ({ navigation }) => {
         </View>
       </View>
       <KeyboardAvoidingView
-        behavior={'position'}
-        keyboardVerticalOffset={Platform.OS == "ios" ? 60 : 80}
+        style={{position: 'absolute', left: 0, right: 0, bottom: 0}}
+        behavior={Platform.OS == "ios"?'position':null}
       >
         <View style={styles.footer}>
           <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()} accessible={false}>
@@ -102,6 +105,7 @@ const TicketScreen = ({ navigation }) => {
                 }}
                 style={styles.input}
                 value={message}
+                multiline
               />
             </View>
           </TouchableWithoutFeedback>
@@ -132,41 +136,53 @@ TicketScreen.navigationOptions = (props) => {
   }
 }
 const styles = StyleSheet.create({
+  headerContainer: {
+    width: '100%',
+    backgroundColor: '#7598BA',
+    height: Dimensions.get('window').height * 0.20,
+    borderBottomLeftRadius: 70,
+    alignItems:'center',
+},
+
+headerText: {
+    color: 'white',
+    fontSize: normalize(40) *
+        Math.min(
+            Dimensions.get("window").height / 900.0,
+            Dimensions.get("window").width / 500.0
+        ),
+        paddingTop:'15%',
+    fontFamily: 'Montserrat_bold'
+},
+
   container: {
     backgroundColor: '#FFFFFF',
     height: Dimensions.get('window').height,
     width: Dimensions.get('window').width,
     flex: 1,
   },
-
   footer: {
     borderWidth: 1,
-    borderTopColor: '#E9EEF1',
+    borderTopColor:'#E9EEF1',
     borderColor: '#FFFFFF',
-    flexDirection: 'row',
     backgroundColor: '#FFFFFF',
-    paddingHorizontal: 10,
-    alignItems: 'center',
-    height: 60,
-    bottom: 0,
-  },
-  inputContainer: {
     flexDirection: 'row',
+    justifyContent: 'space-between',
+    width:'100%',
     alignItems: 'center',
+    height:normalize(60), 
+    paddingHorizontal:'5%'
+  },
+  inputContainer:{
     flex: 1,
-    height: '100%'
   },
   input: {
     fontFamily: 'Montserrat',
     fontSize: 16,
     color: '#BCC5D3',
-    paddingHorizontal: '10%',
-    justifyContent: 'flex-end',
-    bottom: 0,
-    height: '100%',
-    width: '100%',
+    paddingHorizontal:'5%'
   },
-
+  
 })
 
 export default TicketScreen;
